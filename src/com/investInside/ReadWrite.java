57532cbe438a7fd22a262f81a2ce3e7bcb638b65
dmytro <dnkushniruk@gmail.com>
2020-11-25 14:32:56 +0200

    the fourth

--- src/com/investInside/ReadWrite.java
+++ src/com/investInside/ReadWrite.java
@@ -7,20 +7,13 @@ import java.util.TreeMap;
 public class ReadWrite {
     public static String[] readInput() throws IOException {
         File input = new File("input.txt");
-        String[] units = new String(Files.readAllBytes(input.toPath())).split("\n");
-        return units;
+        return new String(Files.readAllBytes(input.toPath())).split("\n");
     }
 
     public static void writeResult(String order) throws IOException {
         File output = new File("output.txt");
         OutputStream os = new FileOutputStream(output, true);
-        try {
-            os.write((order.getBytes()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            os.close();
-        }
-
+        os.write((order.getBytes()));
+        os.close();
     }
 }
